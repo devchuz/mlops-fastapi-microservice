eccfb49040d1320ccd9921be9177537cdbc4f805
extjorincon_belcorp <extjorincon@belcorp.biz>
2026-06-06 00:51:39 -0500

    fix: add app path for Modal Docker image import

--- src/modal_app.py
+++ src/modal_app.py
@@ -1,6 +1,6 @@
 import os
 import modal
-
+import sys
 docker_image = os.environ.get(
     "DOCKER_IMAGE_TAG",
     "devchuz/mlops-fastapi-microservice:latest",
@@ -21,5 +21,8 @@ app = modal.App(modal_app_name)
 @app.function(image=image)
 @modal.asgi_app()
 def fastapi_app():
+    sys.path.insert(0, "/app")
+
     from src.core.server import app as web_app
+
     return web_app
